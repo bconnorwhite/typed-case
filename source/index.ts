4b3e9075fa7c36e524f8a2ef01e4c07e6f47d1dd
Connor White <connor.bcw@gmail.com>
2023-01-09 13:46:49 -0600

    fix: fix build issue

--- source/index.ts
+++ source/index.ts
@@ -1,13 +1,24 @@
 export { capitalize } from "./capitalize.js";
 export { uncapitalize } from "./uncapitalize.js";
 
-export { UpperCase, toUpperCase } from "./upper-case.js";
-export { LowerCase, toLowerCase } from "./lower-case.js";
+export type { UpperCase } from "./upper-case.js";
+export { toUpperCase } from "./upper-case.js";
 
-export { CamelCase, CamelCaseOptions, toCamelCase } from "./camel-case.js";
-export { PascalCase, PascalCaseOptions, toPascalCase } from "./pascal-case.js";
-export { SnakeCase, toSnakeCase } from "./snake-case.js";
-export { KebabCase, toKebabCase } from "./kebab-case.js";
-export { ConstantCase, toConstantCase } from "./constant-case.js";
-export { TrainCase, toTrainCase } from "./train-case.js";
-export { TitleCase, toTitleCase } from "./title-case.js";
+export type { LowerCase } from "./lower-case.js";
+export { toLowerCase } from "./lower-case.js";
+
+export type { CamelCase, CamelCaseOptions } from "./camel-case.js";
+export type { PascalCase, PascalCaseOptions } from "./pascal-case.js";
+export type { SnakeCase } from "./snake-case.js";
+export type { KebabCase } from "./kebab-case.js";
+export type { ConstantCase } from "./constant-case.js";
+export type { TrainCase } from "./train-case.js";
+export type { TitleCase } from "./title-case.js";
+
+export { toCamelCase } from "./camel-case.js";
+export { toPascalCase } from "./pascal-case.js";
+export { toSnakeCase } from "./snake-case.js";
+export { toKebabCase } from "./kebab-case.js";
+export { toConstantCase } from "./constant-case.js";
+export { toTrainCase } from "./train-case.js";
+export { toTitleCase } from "./title-case.js";
