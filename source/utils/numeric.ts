cb25c404c40ed0aa88f4bf32778518ebde5b558c
Connor White <connor.bcw@gmail.com>
2023-01-29 12:33:58 -0600

    refactor: remove IsNumeric

--- source/utils/numeric.ts
+++ source/utils/numeric.ts
@@ -2,9 +2,3 @@
 export type Digit = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9";
 
 export type IsDigit<T extends string> = T extends Digit ? true : false;
-
-export type IsNumeric<T extends string> = T extends `${number}`
-  ? T extends " "
-    ? false
-    : true
-  : false;
